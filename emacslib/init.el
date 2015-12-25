57aec64bf3d1038f3f7a462665e7d95be0722419
xiaoanh <xiaoanhuang@163.com>
2015-12-25 16:58:45 +0800

    @auto init.el to init dotemacsw.el

---
+++ emacslib/init.el
@@ -0,0 +1,27 @@
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(setq org-dir (expand-file-name "lisp" (expand-file-name "org-mode-master" dotfiles-dir)))
+(setq org-contrib-dir (expand-file-name "lisp" (expand-file-name "contrib" org-dir)))
+;; # (let* ((org-dir (expand-file-name
+;; #                  "lisp" (expand-file-name
+;; #                          "org" (expand-file-name
+;; #                                 "src" dotfiles-dir))))
+;; #        (org-contrib-dir (expand-file-name
+;; #                          "lisp" (expand-file-name
+;; #                                  "contrib" (expand-file-name
+;; #                                             ".." org-dir))))
+;; #        (load-path (append (list org-dir org-contrib-dir)
+;; #                           (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+(require 'org-install)
+(require 'ob-tangle)
+(setq initial-user-file (concat dotfiles-dir "dotemacsw.el"))
+;; load up all literate org-mode files in this directory
+; (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(org-babel-load-file (concat dotfiles-dir "dotemacsw.org"))
+;;; init.el ends here
